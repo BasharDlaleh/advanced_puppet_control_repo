70a8cbf1188a55a5d0a65a2df84f987e76c37512
BasharDlaleh <66019475+BasharDlaleh@users.noreply.github.com>
2023-11-01 15:18:48 +0300

    Update master.pp

--- site/roles/manifests/master.pp
+++ site/roles/manifests/master.pp
@@ -1,3 +1,4 @@
+# note that r10k webhook didn't work because we used the same master vm from the Beginner course whioch was already configured maually
 class role::master {
   include profile::r10k
 }
