0f821cf263a3725b10dbd68589fbfbeb0b7f8872
BasharDlaleh <66019475+BasharDlaleh@users.noreply.github.com>
2023-11-01 16:28:45 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,9 +1,9 @@
 node 'master.puppet.vm' {
   include role::master
-  file {'/etc/secret_password.txt':
-    ensure => file,
-    content => lookup('secret_password'),
-  }
+ # file {'/etc/secret_password.txt':
+ #   ensure => file,
+ #   content => lookup('secret_password'),
+ # }
   file {'/etc/test.txt':
     ensure => file,
     content => 'test file',
