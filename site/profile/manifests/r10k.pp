4a156ad11a49a1362790ebba3849391891f107fd
BasharDlaleh <66019475+BasharDlaleh@users.noreply.github.com>
2023-11-01 14:09:02 +0300

    Create r10k.pp

---
+++ site/profile/manifests/r10k.pp
@@ -0,0 +1,17 @@
+# this class is for creating a webhook that auto pulles repo changes on master server so we don't have to pull the changes manually with "r10k deploy environment -p" command
+class profile::r10k {
+  #below we are using some classes from the r10k module
+  class {'r10k':
+    remote => 'https://github.com/BasharDlaleh/puppet_control_repo'
+  }
+  class {'r10k::webhook::config':
+    use_mcollective => false,
+    # disabling ssl for simplicity
+    enable_ssl => false,
+  }
+  #because this is an open source server, there's two parameters required here, user root and group root
+  class {'r10k::webhook':
+    user  => 'root',
+    group => 'root',
+  }
+}
