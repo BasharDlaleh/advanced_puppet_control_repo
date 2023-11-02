9e4ecd3c36030b646f46e04a440bce3a837e9cf6
BasharDlaleh <66019475+BasharDlaleh@users.noreply.github.com>
2023-11-02 14:45:28 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,3 +1,4 @@
+# note that we used the command #puppet agent -t --environment=advanced to deploy the 'advanced' environment configuration instead of the default 'production' environment
 node 'master.puppet.vm' {
   #include role::master
   file {'/etc/secret_password.txt':
